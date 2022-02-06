442a0c570b30511c792185d21cc1c8356033b840
victoria mwangi <victoriamwangi021@gmail.com>
2022-02-06 17:40:39 +0300

    feat:form-validation check if the user has selected  any value

--- js/script.js
+++ js/script.js
@@ -10,14 +10,38 @@ for (var x = 0; x < tabs_main.length; x++) {
 
 for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function (e) {
+
+        var id = e.target.dataset.target;
+        var question = e.target.dataset.question;
+        var answer = e.target.dataset.answer;
+        var selection = "";
+
+        var radios = document.getElementsByClassName(question);
+
+        for (var radio of radios) {
+            if (radio.checked) {
+                selection = radio.value;
+            }
+
+        }
+
+        //validating inputs
+        if (selection == "") {
+            alert("Kindly select a value from the provided options");
+            return false;
+        }
+
+        //switch tabs
         var tabs = document.querySelectorAll('.tabs');
         for (var t = 0; t < tabs.length; t++) {
             tabs[t].classList.remove('active');
         }
-
-        var id = e.target.dataset.target;
-        var nextTab = document.getElementById(id);
-
-        nextTab.classList.add('active');
+        if (id == "submit") {
+            document.querySelector('result-tab').classList.add('active');
+        } else {
+            var nextTab = document.getElementById(Id);
+            nextTab.classList.add('active');
+        }
     });
 }
+
